78ef76299c8bf7658f142c770e80a9dc8f117c9a
BumTiger <jbpark03@gmail.com>
2025-10-15 14:08:16 +0900

    generates order ID prefix of 6 random alpha-numeric char

---
+++ src/util/service.js
@@ -0,0 +1,21 @@
+const ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const NUMERIC = "0123456789_-";
+const CHARS = ALPHA + NUMERIC;
+
+export default function getOrderIdPrefix(length) {
+  // Use crypto.getRandomValues for better security in browsers
+  const array = new Uint32Array(length);
+  crypto.getRandomValues(array);
+  
+  let result = '';
+
+  // 첫 문자는 알파벳에서 임의 선택
+  const randomIndex = array[0] % ALPHA.length;
+  result += ALPHA.charAt(randomIndex);
+
+  for (let i = 1; i < length; i++) {
+    const randomIndex = array[i] % CHARS.length;
+    result += CHARS.charAt(randomIndex);
+  }
+  return result;
+}

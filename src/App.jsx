c938a1a91a05c2a133bcb6fb0ae7f2bd39ed56eb
BumTiger <jbpark03@gmail.com>
2025-10-15 14:11:55 +0900

    shows purchase summary on top of payment page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import getOrderIdPrefix from "./util/service";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const amount = 10700;
+  const productName = "백설공주 2개 등";
+  const orderId = getOrderIdPrefix(6);
 
   return (
     <>
+      <h1>결제 창</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul style={{ textAlign: "left" }}>
+          <li>내역: {productName}</li>
+          <li>금액: {amount}원</li>
+          <li>주문ID: {orderId}00000001</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

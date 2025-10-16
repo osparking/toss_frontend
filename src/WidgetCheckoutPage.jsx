52eab4b6abae9243dda9d7594b16c24e47a149f2
BumTiger <jbpark03@gmail.com>
2025-10-16 11:19:46 +0900

    copy and paste upper half of return statement

--- src/WidgetCheckoutPage.jsx
+++ src/WidgetCheckoutPage.jsx
@@ -44,17 +44,65 @@ function WidgetCheckoutPage() {
     fetchPaymentWidgets();
   }, [clientKey, customerKey]);
 
+  const [ready, setReady] = useState(false);
+
+  // return (
+  //   <>
+  //     <h1>결제 창</h1>
+  //     <div>
+  //       <ul style={{ textAlign: "left" }}>
+  //         <li>내역: {productName}</li>
+  //         <li>금액: {amount.value.toLocaleString()}원</li>
+  //         <li>주문ID: {orderId}00000001</li>
+  //       </ul>
+  //     </div>
+  //   </>
+  // );
+
   return (
-    <>
-      <h1>결제 창</h1>
-      <div>
-        <ul style={{ textAlign: "left" }}>
-          <li>내역: {productName}</li>
-          <li>금액: {amount.value.toLocaleString()}원</li>
-          <li>주문ID: {orderId}00000001</li>
-        </ul>
+    <div className="wrapper">
+      <div className="box_section">
+        {/* 결제 UI */}
+        <div id="payment-method" />
+        {/* 이용약관 UI */}
+        <div id="agreement" />
+        {/* 쿠폰 체크박스 */}
+        <div style={{ paddingLeft: "30px" }}>
+          <div className="checkable typography--p">
+            <label
+              htmlFor="coupon-box"
+              className="checkable__label typography--regular"
+            >
+              <input
+                id="coupon-box"
+                className="checkable__input"
+                type="checkbox"
+                aria-checked="true"
+                disabled={!ready}
+                // ------  주문서의 결제 금액이 변경되었을 경우 결제 금액 업데이트 ------
+                // @docs https://docs.tosspayments.com/sdk/v2/js#widgetssetamount
+                onChange={async (event) => {
+                  if (event.target.checked) {
+                    await widgets.setAmount({
+                      currency: amount.currency,
+                      value: amount.value - 5000,
+                    });
+
+                    return;
+                  }
+
+                  await widgets.setAmount({
+                    currency: amount.currency,
+                    value: amount.value,
+                  });
+                }}
+              />
+              <span className="checkable__label-text">5,000원 쿠폰 적용</span>
+            </label>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 

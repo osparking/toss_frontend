d886e58e2ef7c4c9a5f073b80ea6e194aa0bb0f8
BumTiger <jbpark03@gmail.com>
2025-10-16 10:42:03 +0900

    defines two key constants and thousand comma

--- src/WidgetCheckoutPage.jsx
+++ src/WidgetCheckoutPage.jsx
@@ -2,6 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import getOrderIdPrefix from "./util/service";
 
+// 전자결제 신청 및 가입 완료 후, clientKey 를 다음으로 수정할 것.
+// 개발자센터의 결제위젯 연동 키 > 클라이언트 키
+const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+const customerKey = generateRandomString();
+
+function generateRandomString() {
+  return window.btoa(Math.random().toString()).slice(0, 20);
+}
+
 function WidgetCheckoutPage() {
   const [amount, setAmount] = useState({
     currency: "KRW",
@@ -16,7 +25,7 @@ function WidgetCheckoutPage() {
       <div>
         <ul style={{ textAlign: "left" }}>
           <li>내역: {productName}</li>
-          <li>금액: {amount.value}원</li>
+          <li>금액: {amount.value.toLocaleString()}원</li>
           <li>주문ID: {orderId}00000001</li>
         </ul>
       </div>

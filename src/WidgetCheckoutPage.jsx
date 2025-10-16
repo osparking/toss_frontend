4187dda229f932a3220ce475f695b6d0ac3eaf77
BumTiger <jbpark03@gmail.com>
2025-10-16 09:25:38 +0900

    pay widget displayed pay processed well

---
+++ src/WidgetCheckoutPage.jsx
@@ -0,0 +1,169 @@
+import { loadTossPayments } from "@tosspayments/tosspayments-sdk";
+import { useEffect, useState } from "react";
+import "./App.css";
+import getOrderIdPrefix from "./util/service";
+
+// 전자결제 신청 및 가입 완료 후, clientKey 를 다음으로 수정할 것.
+// 개발자센터의 결제위젯 연동 키 > 클라이언트 키
+const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+const customerKey = generateRandomString();
+
+function App() {
+  const [amount, setAmount] = useState({
+    currency: "KRW",
+    value: 10700,
+  });
+
+  const [ready, setReady] = useState(false);
+  const [widgets, setWidgets] = useState(null);
+
+  useEffect(() => {
+    async function renderPaymentWidgets() {
+      if (widgets == null) {
+        return;
+      }
+      // @docs https://docs.tosspayments.com/sdk/v2/js#widgetssetamount
+      await widgets.setAmount(amount);
+
+      await Promise.all([
+        // ------  결제 UI 렌더링 ------
+        // @docs https://docs.tosspayments.com/sdk/v2/js#widgetsrenderpaymentmethods
+        widgets.renderPaymentMethods({
+          selector: "#payment-method",
+          // 렌더링하고 싶은 결제 UI의 variantKey
+          // 결제 수단 및 스타일이 다른 멀티 UI를 직접 만들고 싶다면 계약이 필요해요.
+          // @docs https://docs.tosspayments.com/guides/v2/payment-widget/admin#새로운-결제-ui-추가하기
+          variantKey: "DEFAULT",
+        }),
+        // ------  이용약관 UI 렌더링 ------
+        // @docs https://docs.tosspayments.com/sdk/v2/js#widgetsrenderagreement
+        widgets.renderAgreement({
+          selector: "#agreement",
+          variantKey: "AGREEMENT",
+        }),
+      ]);
+
+      setReady(true);
+    }
+
+    renderPaymentWidgets();
+  }, [widgets]);
+
+  useEffect(() => {
+    async function fetchPaymentWidgets() {
+      try {
+        const tossPayments = await loadTossPayments(clientKey);
+
+        // 회원 결제
+        // @docs https://docs.tosspayments.com/sdk/v2/js#tosspaymentswidgets
+        const widgets = tossPayments.widgets({
+          customerKey,
+        });
+        // 비회원 결제
+        // const widgets = tossPayments.widgets({ customerKey: ANONYMOUS });
+
+        setWidgets(widgets);
+      } catch (error) {
+        console.error("Error fetching payment widget:", error);
+      }
+    }
+
+    fetchPaymentWidgets();
+  }, [clientKey, customerKey]);
+
+  const productName = "백설공주 2개 등";
+  const orderId = getOrderIdPrefix(6);
+
+  return (
+    <div className="wrapper">
+      <div className="box_section"></div>
+      <div className="box_section">
+        <h3>구매 내역 요약</h3>
+        <ul
+          style={{ textAlign: "left", border: "1px solid black", width: "90%", margin: "5%"}}
+        >
+          <li>내역: {productName}</li>
+          <li>금액: {amount.value.toLocaleString()}원</li>
+          <li>주문ID: {orderId}00000001</li>
+        </ul>
+        {/* 결제 UI */}
+        <div id="payment-method" />
+        {/* 이용약관 UI */}
+        <div id="agreement" />
+        {/* 쿠폰 체크박스 */}
+        <div style={{ paddingLeft: "30px" }}>
+          <div className="checkable typography--p">
+            <label
+              htmlFor="coupon-box"
+              className="checkable__label typography--regular"
+            >
+              <input
+                id="coupon-box"
+                className="checkable__input"
+                type="checkbox"
+                aria-checked="true"
+                disabled={!ready}
+                // ------  주문서의 결제 금액이 변경되었을 경우 결제 금액 업데이트 ------
+                // @docs https://docs.tosspayments.com/sdk/v2/js#widgetssetamount
+                onChange={async (event) => {
+                  if (event.target.checked) {
+                    await widgets.setAmount({
+                      currency: amount.currency,
+                      value: amount.value - 1070,
+                    });
+
+                    return;
+                  }
+
+                  await widgets.setAmount({
+                    currency: amount.currency,
+                    value: amount.value,
+                  });
+                }}
+              />
+              <span className="checkable__label-text">5,000원 쿠폰 적용</span>
+            </label>
+          </div>
+        </div>
+
+        {/* 결제하기 버튼 */}
+        <button
+          className="button"
+          style={{ marginTop: "30px" }}
+          disabled={!ready}
+          // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
+          // @docs https://docs.tosspayments.com/sdk/v2/js#widgetsrequestpayment
+          onClick={async () => {
+            try {
+              // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+              // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
+              await widgets.requestPayment({
+                orderId: `${orderId}00000001`, //generateRandomString(), // 고유 주문 번호
+                orderName: `${ productName }`,
+                successUrl: window.location.origin + "/widget/success", // 결제 요청이 성공하면 리다이렉트되는 URL
+                failUrl: window.location.origin + "/fail", // 결제 요청이 실패하면 리다이렉트되는 URL
+                customerEmail: "customer123@gmail.com",
+                customerName: "김비누",
+                // 가상계좌 안내, 퀵계좌이체 휴대폰 번호 자동 완성에 사용되는 값입니다. 필요하다면 주석을 해제해 주세요.
+                // customerMobilePhone: "01012341234",
+              });
+            } catch (error) {
+              // 에러 처리하기
+              console.error(error);
+            }
+          }}
+        >
+          결제하기
+        </button>
+      </div>
+    </div>
+  );
+
+  return <></>;
+}
+
+export default App;
+
+function generateRandomString() {
+  return window.btoa(Math.random().toString()).slice(0, 20);
+}
